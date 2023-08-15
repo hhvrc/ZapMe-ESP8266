03194f2db5197d57e3557ebe2efc2e2aa3b34580
hhvrc <github@heavenvr.tech>
2023-08-15 06:16:18 +0200

    Optimize loggers

--- include/logger.hpp
+++ include/logger.hpp
@@ -22,12 +22,12 @@ public:
   static void println(const char* message);
   static void println();
 
-  static void printhexln(const char* message, const std::uint8_t* data, std::size_t size);
-  static void printhexln(const char* message, std::span<const std::uint8_t> data) {
-    printhexln(message, data.data(), data.size());
-  }
   static void printhexln(const std::uint8_t* data, std::size_t size);
   static void printhexln(std::span<const std::uint8_t> data) {
     printhexln(data.data(), data.size());
+  }
+  static void printhexln(const char* message, const std::uint8_t* data, std::size_t size);
+  static void printhexln(const char* message, std::span<const std::uint8_t> data) {
+    printhexln(message, data.data(), data.size());
   }
 };
--- src/logger.cpp
+++ src/logger.cpp
@@ -2,11 +2,58 @@
 
 #include "sdcard.hpp"
 
+#include <fmt/format.h>
+
 constexpr bool LOG_TO_SERIAL = true;
-#define SERIAL_BEGIN(...) if (LOG_TO_SERIAL) Serial.begin(__VA_ARGS__)
-#define SERIAL_PRINT(...) if (LOG_TO_SERIAL) Serial.print(__VA_ARGS__)
-#define SERIAL_PRINTF(...) if (LOG_TO_SERIAL) Serial.printf(__VA_ARGS__)
-#define SERIAL_PRINTLN(...) if (LOG_TO_SERIAL) Serial.println(__VA_ARGS__)
+#define SERIAL_BEGIN(...) \
+  if (LOG_TO_SERIAL) { \
+    Serial.begin(__VA_ARGS__); \
+  }
+#define SERIAL_PRINT(buf) \
+  if (LOG_TO_SERIAL) { \
+    Serial.print(buf); \
+  }
+#define SERIAL_PRINTS(buf, len) \
+  if (LOG_TO_SERIAL) { \
+    Serial.write(buf, len); \
+  }
+#define SERIAL_PRINTF(...) \
+  if (LOG_TO_SERIAL) { \
+    Serial.printf(__VA_ARGS__); \
+  }
+#define SERIAL_PRINTLN(buf) \
+  if (LOG_TO_SERIAL) { \
+    Serial.println(buf); \
+  }
+#define SERIAL_PRINTELN() \
+  if (LOG_TO_SERIAL) { \
+    Serial.println(); \
+  }
+#define SERIAL_PRINTSLN(buf, len) \
+  if (LOG_TO_SERIAL) { \
+    Serial.write(buf, len); \
+    Serial.println(); \
+  }
+
+#define LOGGER_PRINT(buf) \
+  SERIAL_PRINT(buf) \
+  file.print(buf)
+#define LOGGER_PRINTS(buf, len) \
+  SERIAL_PRINTS(buf, len) \
+  file.write(buf, len)
+#define LOGGER_PRINTF(...) \
+  SERIAL_PRINTF(__VA_ARGS__) \
+  file.printf(__VA_ARGS__)
+#define LOGGER_PRINTLN(buf) \
+  SERIAL_PRINTLN(buf) \
+  file.println(buf)
+#define LOGGER_PRINTELN() \
+  SERIAL_PRINTELN(buf) \
+  file.println()
+#define LOGGER_PRINTSLN(buf, len) \
+  SERIAL_PRINTSLN(buf, len) \
+  file.write(buf, len); \
+  file.println()
 
 std::uint64_t LogIndex = 0;
 char FileName[32] = {0};
@@ -17,7 +64,7 @@ Logger::InitializationError Logger::Initialize() {
   }
 
   SERIAL_BEGIN(115200);
-  SERIAL_PRINTLN();
+  SERIAL_PRINTELN();
 
   auto sd = SDCard::GetInstance();
   if (!sd->ok()) {
@@ -59,7 +106,6 @@ Logger::InitializationError Logger::Initialize() {
 }
 
 #define GET_FILE \
-  std::uint64_t milli = millis(); \
   auto sd = SDCard::GetInstance(); \
   if (!sd->ok()) { \
     return; \
@@ -69,10 +115,9 @@ Logger::InitializationError Logger::Initialize() {
     return; \
   }
 
-void PreLog(FsFile& file, std::uint64_t milli) {
-  // Print the message as [DAYS:HOURS:MINUTES:SECONDS.MILLIS] message (0 padded 2 digits, except for millis which is 3 digits)
-  std::uint64_t seconds = milli / 1000;
-  milli -= seconds * 1000;
+int FormatTimestamp(char* buffer, std::size_t bufferSize, std::uint64_t millis) {
+  std::uint64_t seconds = millis / 1000;
+  millis -= seconds * 1000;
 
   std::uint64_t minutes = seconds / 60;
   seconds -= minutes * 60;
@@ -83,96 +128,198 @@ void PreLog(FsFile& file, std::uint64_t milli) {
   std::uint64_t days = hours / 24;
   hours -= days * 24;
 
-  file.printf("[%02llu:%02llu:%02llu:%02llu.%03llu] ", days, hours, minutes, seconds, milli);
-  SERIAL_PRINTF("[%02llu:%02llu:%02llu:%02llu.%03llu] ", days, hours, minutes, seconds, milli);
+  return snprintf(buffer, bufferSize, "[%02hu:%02hhu:%02hhu:%02hhu.%03hu] ",
+    static_cast<std::uint16_t>(days),
+    static_cast<std::uint8_t>(hours),
+    static_cast<std::uint8_t>(minutes),
+    static_cast<std::uint8_t>(seconds),
+    static_cast<std::uint16_t>(millis)
+  );
+}
+int PrintTimestamp(FsFile& file, std::uint64_t millis) {
+  char buffer[32];
+  int len = FormatTimestamp(buffer, sizeof(buffer), millis);
+  LOGGER_PRINTS(buffer, len);
+  return len;
 }
 
 void Logger::vprintlnf(const char* format, va_list args) {
-  char temp[64];
-  char* buffer = temp;
-  std::size_t len = vsnprintf(temp, sizeof(temp), format, args);
-  if (len > sizeof(temp) - 1) {
-      buffer = new char[len + 1];
-      if (!buffer) {
-          return;
-      }
-      vsnprintf(buffer, len + 1, format, args);
+  std::uint64_t milli = millis();
+  GET_FILE
+
+  char buffer[64];
+  char* ptr = buffer;
+
+  int tsLen = FormatTimestamp(buffer, sizeof(buffer), milli);
+  int logLen = vsnprintf(buffer + tsLen, sizeof(buffer) - tsLen, format, args);
+
+  int len = tsLen + logLen;
+  if (len > static_cast<int>(sizeof(buffer)) - 1) {
+    ptr = new char[len + 1];
+    if (!ptr) {
+      return;
+    }
+
+    tsLen = FormatTimestamp(ptr, len + 1, milli);
+    logLen = vsnprintf(ptr + tsLen, len + 1, format, args);
+    len = tsLen + logLen;
   }
 
-  Logger::println(buffer);
+  LOGGER_PRINTSLN(ptr, len);
 
-  if (buffer != temp) {
-      delete[] buffer;
+  if (ptr != buffer) {
+      delete[] ptr;
   }
+
+  file.close();
 }
 void Logger::printlnf(const char* format, ...) {
-  char temp[64];
-  char* buffer = temp;
-  va_list args;
+  std::uint64_t milli = millis();
+  GET_FILE
 
+  char buffer[64];
+  char* ptr = buffer;
+
+  int tsLen = FormatTimestamp(buffer, sizeof(buffer), milli);
+
+  va_list args;
   va_start(args, format);
-  std::size_t len = vsnprintf(temp, sizeof(temp), format, args);
+  int logLen = vsnprintf(buffer + tsLen, sizeof(buffer) - tsLen, format, args);
   va_end(args);
 
-  if (len > sizeof(temp) - 1) {
-      buffer = new char[len + 1];
-      if (!buffer) {
-          return;
-      }
+  int len = tsLen + logLen;
+  if (len > static_cast<int>(sizeof(buffer)) - 1) {
+    ptr = new char[len + 1];
+    if (!ptr) {
+      return;
+    }
 
-      va_start(args, format);
-      vsnprintf(buffer, len + 1, format, args);
-      va_end(args);
+    tsLen = FormatTimestamp(ptr, len + 1, milli);
+    va_start(args, format);
+    logLen = vsnprintf(ptr + tsLen, len + 1, format, args);
+    va_end(args);
+    len = tsLen + logLen;
   }
 
-  Logger::println(buffer);
+  LOGGER_PRINTSLN(ptr, len);
 
-  if (buffer != temp) {
-      delete[] buffer;
+  if (ptr != buffer) {
+      delete[] ptr;
   }
+
+  file.close();
 }
 void Logger::println(const String& message) {
+  std::uint64_t milli = millis();
   GET_FILE
-  PreLog(file, milli);
-  file.println(message);
-  SERIAL_PRINTLN(message);
+  PrintTimestamp(file, milli);
+  LOGGER_PRINTLN(message);
   file.close();
 }
 void Logger::println(const char* message) {
+  std::uint64_t milli = millis();
   GET_FILE
-  PreLog(file, milli);
-  file.println(message);
-  SERIAL_PRINTLN(message);
+  PrintTimestamp(file, milli);
+  LOGGER_PRINTLN(message);
   file.close();
 }
 void Logger::println() {
   GET_FILE
-  file.println();
-  SERIAL_PRINTLN();
+  LOGGER_PRINTLN();
   file.close();
 }
 
-void Logger::printhexln(const char* message, const std::uint8_t* data, std::size_t size) {
-  GET_FILE
-  PreLog(file, milli);
-  file.print(message);
-  SERIAL_PRINT(message);
-  for (std::size_t i = 0; i < size; i++) {
-    file.printf("%02X", data[i]);
-    SERIAL_PRINTF("%02X", data[i]);
+constexpr char hexfmtnibble(std::uint8_t data) {
+  data &= 0x0F;
+  if (data < 10) {
+    return '0' + data;
   }
-  file.println();
-  SERIAL_PRINTLN();
+  return 'A' + (data - 10);
+}
+std::size_t hexfmt(char* buffer, std::size_t bufferLen, const std::uint8_t* data, std::size_t size) {
+  const std::size_t hexlen = size * 2;
+
+  if (bufferLen < hexlen) {
+    return hexlen;
+  }
+
+  for (std::size_t i = 0; i < size; i++) {
+    buffer[(i * 2) + 0] = hexfmtnibble(data[i] >> 4);
+    buffer[(i * 2) + 1] = hexfmtnibble(data[i] >> 0);
+  }
+
+  return hexlen;
+}
+
+void Logger::printhexln(const std::uint8_t* data, std::size_t size) {
+  std::uint64_t milli = millis();
+  GET_FILE
+
+  char buffer[64];
+  char* ptr = buffer;
+
+  int tsLen = FormatTimestamp(buffer, sizeof(buffer), milli);
+  int hexLen = size * 2;
+  int len = tsLen + hexLen + 2;
+
+  if (len > static_cast<int>(sizeof(buffer)) - 1) {
+    ptr = new char[len + 1];
+    if (!ptr) {
+      return;
+    }
+
+    tsLen = FormatTimestamp(ptr, len + 1, milli);
+    hexLen = hexfmt(ptr + tsLen, len + 1, data, size);
+  } else {
+    hexLen = hexfmt(buffer + tsLen, sizeof(buffer) - tsLen, data, size);
+  }
+  buffer[tsLen + hexLen + 0] = '\r';
+  buffer[tsLen + hexLen + 1] = '\n';
+  len = tsLen + hexLen + 2;
+
+  LOGGER_PRINTS(buffer, len);
+
+  if (ptr != buffer) {
+      delete[] ptr;
+  }
+
   file.close();
 }
-void Logger::printhexln(const std::uint8_t* data, std::size_t size) {
+void Logger::printhexln(const char* message, const std::uint8_t* data, std::size_t size) {
+  std::uint64_t milli = millis();
   GET_FILE
-  PreLog(file, milli);
-  for (std::size_t i = 0; i < size; i++) {
-    file.printf("%02X", data[i]);
-    SERIAL_PRINTF("%02X", data[i]);
+
+  char buffer[64];
+  char* ptr = buffer;
+
+  int tsLen = FormatTimestamp(buffer, sizeof(buffer), milli);
+  int msgLen = strlen(message);
+  int hexLen = size * 2;
+  int len = tsLen + msgLen + hexLen + 2;
+
+  if (len > static_cast<int>(sizeof(buffer)) - 1) {
+    ptr = new char[len + 1];
+    if (!ptr) {
+      return;
+    }
+
+    tsLen = FormatTimestamp(ptr, len + 1, milli);
+    memcpy(ptr + tsLen, message, msgLen);
+    hexLen = hexfmt(ptr + tsLen + msgLen, len + 1, data, size);
+  } else {
+    memcpy(buffer + tsLen, message, msgLen);
+    hexLen = hexfmt(buffer + tsLen + msgLen, sizeof(buffer) - tsLen - msgLen, data, size);
   }
-  file.println();
-  SERIAL_PRINTLN();
+
+  buffer[tsLen + msgLen + hexLen + 0] = '\r';
+  buffer[tsLen + msgLen + hexLen + 1] = '\n';
+  len = tsLen + msgLen + hexLen + 2;
+
+  LOGGER_PRINTS(buffer, len);
+
+  if (ptr != buffer) {
+      delete[] ptr;
+  }
+
   file.close();
 }
